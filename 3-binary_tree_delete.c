7e0f9b424117789bb8bd03dc4c6162d59cc4bd09
FRIDAUS <ayobamifridaus@gmail.com>
2023-09-04 17:47:06 +0100

    Delete

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - a unction that deletes an entire binary tree
+ * @tree: address of the parent node at level 0
+ * Return: Allow success
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+	else
+	{
+		if (tree != NULL)
+		{
+			binary_tree_delete(tree->left);
+			binary_tree_delete(tree->right);
+		}
+		free(tree);
+	}
+}
