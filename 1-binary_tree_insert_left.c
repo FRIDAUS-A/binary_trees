6498e2955116dee3515f34c3b1deeb298d8443fb
FRIDAUS <ayobamifridaus@gmail.com>
2023-09-04 14:10:48 +0100

    Insert left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,7 +14,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	node = binary_tree_node(parent, value);
 
 	if (parent == NULL)
-		return (NULL);
+		return (node);
 
 	if (!(parent->left))
 		parent->left = node;
