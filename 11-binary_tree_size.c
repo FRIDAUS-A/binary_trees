044076f5e57cf03d44e7f43755aba41d464d7dfe
FRIDAUS <ayobamifridaus@gmail.com>
2023-09-04 23:08:48 +0100

    size

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * size_t binary_tree_size - a function that measures the
+ * size of a binary tree
+ * @tree: address of the node
+ * Return: Allow Success
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size;
+
+	if (tree == NULL)
+		return (0);
+
+	size = binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1;
+	return (size);
+}
