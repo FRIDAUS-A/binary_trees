ce09d3c091251c27e666abeb5fe9e0b26b695a66
FRIDAUS <ayobamifridaus@gmail.com>
2023-09-04 11:43:16 +0100

    New node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_node - create a new node
+ * @parent: address of the parent node
+ * @value: value in the node
+ * Return: Allow success
+ */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node;
